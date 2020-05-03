0de23f517ec8c708a58d1c311410618d497d2220
HimanshuYadav <2019kuec2009@iiitkota.ac.in>
2020-05-04 00:13:26 +0530

    Improved UI design and fixed some errors

---
+++ README.md
@@ -0,0 +1,15 @@
+# About 
+Text to Speech built on the top of SpeechSynthesis Web API.
+
+# Getting Started
+You can check the web version of this app working straight into your browser by [`clicking here`]('https://himanshu12345yadav.github.io/Text-To-Speech').
+
+**Wanna give this app your own flavour, feel free to do so by cloning this app in your system**
+
+```bash
+git clone https://github.com/himanshu12345yadav/Text-To-Speech.git
+cd Text-To-Speech
+```
+Inside the project folder run the `index.html` file or just this project folder on your localhost server and you are ready to go.
+
+`Happy hacking!`
---
+++ index.js
@@ -0,0 +1,73 @@
+const pitch = document.querySelector('#pitch');
+const speed = document.querySelector('#speed');
+const voice = document.querySelector('#voice');
+const submit = document.querySelector('#submit');
+const text = document.querySelector('#text');
+const pitch_badge = document.querySelector('#pitch-badge');
+const speed_badge = document.querySelector('#speed-badge');
+var check_status = 1;
+let synth = window.speechSynthesis;
+let voices = [];
+synth.onvoiceschanged = () => {
+    if (check_status) {
+        voices = synth.getVoices();
+        voices.forEach((voice_name) => {
+            const option = document.createElement('option');
+            if (voice_name.name === 'Google US English') {
+                option.selected = true;
+            }
+            option.setAttribute('data-name', voice_name.name);
+            option.setAttribute('data-lang', voice_name.lang);
+            option.textContent =
+                voice_name.name + ' ( ' + voice_name.lang + ' ) ';
+            voice.appendChild(option);
+            check_status = 0;
+        });
+    }
+};
+
+pitch.onchange = () => {
+    pitch_badge.innerText = pitch.value;
+};
+speed.onchange = () => {
+    speed_badge.innerText = speed.value;
+};
+
+submit.onclick = (event) => {
+    event.preventDefault();
+    if (text.value === '') {
+        text.classList.add('is-invalid');
+    } else {
+        if (text.classList.contains('is-invalid')) {
+            text.classList.remove('is-invalid');
+            text.classList.add('is-valid');
+        }
+        var speak = new SpeechSynthesisUtterance(text.value);
+        const selected_voice = voice.selectedOptions[0].getAttribute(
+            'data-name'
+        );
+        voices.forEach((item) => {
+            if (item.name === selected_voice) {
+                speak.voice = item;
+            }
+        });
+        var spinner = document.createElement('span');
+        var form_group = document.querySelector('.form-group');
+        spinner.setAttribute(
+            'class',
+            'spinner-border text-light spinner'
+        );
+        form_group.appendChild(spinner);
+        text.disabled = true;
+        speak.pitch = pitch.value;
+        speak.rate = speed.value;
+        speak.onstart = () => {
+            form_group.removeChild(spinner);
+        };
+        synth.cancel();
+        synth.speak(speak);
+        speak.onend = () => {
+            text.disabled = false;
+        };
+    }
+};
